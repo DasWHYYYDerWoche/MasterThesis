223212d8923bd16cc8cc724ae926fa3bd696f2f8
Felix Espey <felix.espey@piky.de>
2026-05-11 17:10:18 +0200

    fix: simulation gap now heavily punishes missing logs to prevent optimization from just trying to crash all logs.

--- src/Optimizer/SimOptimizer.py
+++ src/Optimizer/SimOptimizer.py
@@ -219,8 +219,8 @@ class SimOptimizer:
         # Run simulation
         gap_handler = simulator.simulation_gap(simulator_parameters, experiments,
                                                replay_mode=ExperimentMode.DEL_EXISTING)
-        if len(gap_handler.invalid_logs) > 0:
-            logger.warning("%s invalid logs found: ", len(gap_handler.invalid_logs), gap_handler.invalid_logs)
+        if gap_handler.num_invalid_logs > 0:
+            logger.warning("%s invalid logs found: ", gap_handler.num_invalid_logs, gap_handler.num_invalid_logs)
         gap = gap_handler.get_final_FINAL_gap_avg(lambda gap_object: SimulationGapData.get_total_gap_avg(gap_object))
         # Return tuple (DEAP requirement)
         return (gap,)
